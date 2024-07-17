407964d5ff20549c25decd3736eec1f96559a98d
Hiroto Sasagawa <nagutabby@nagutabby.uk>
2024-07-18 00:03:59 +0900

    インデントを整える

--- src/main/java/main/control/ResvController.java
+++ src/main/java/main/control/ResvController.java
@@ -5,68 +5,69 @@ import main.model.ResvModel;
 import main.view.*;
 
 public class ResvController {
-	private static ResvController instance;
-	private ResvController() {}
+    private static ResvController instance;
+    private ResvController() {}
 
-	public static ResvController getInstance() {
-		if (instance == null)
-			instance = new ResvController();
-		return instance;
-	}
+    public static ResvController getInstance() {
+        if (instance == null) {
+            instance = new ResvController();
+        }
+        return instance;
+    }
 
-	private ResvModel model = ResvModel.getInstance();
+    private ResvModel model = ResvModel.getInstance();
 
-	private ResvView resvView;
-	private RoomView roomView;
-	private MessageView messageView;
+    private ResvView resvView;
+    private RoomView roomView;
+    private MessageView messageView;
 
-	public ResvView getResvView() { return resvView; }
-	public RoomView getRoomView() { return roomView; }
-	public MessageView getMessageView() { return messageView; }
+    public ResvView getResvView() { return resvView; }
+    public RoomView getRoomView() { return roomView; }
+    public MessageView getMessageView() { return messageView; }
 
-	public void setResvView(ResvView v) {resvView = v;}
-	public void setRoomView(RoomView v) {roomView = v;}
-	public void setMessageView(MessageView v) {messageView = v;}
+    public void setResvView(ResvView v) {resvView = v;}
+    public void setRoomView(RoomView v) {roomView = v;}
+    public void setMessageView(MessageView v) {messageView = v;}
 
-	//日付検査のみ
-	private boolean valid(Reservation r) {
-		int ckin  = Util.toInt(r.ckin);
-		int ckout = Util.toInt(r.ckout);
-		return (Util.inRange(ckin,1,20) &&
-				Util.inRange(ckout-1,1,20) &&
-				ckin < ckout);
-	}
+    // 日付検査のみ
+    private boolean valid(Reservation r) {
+        int ckin  = Util.toInt(r.ckin);
+        int ckout = Util.toInt(r.ckout);
+        return (Util.inRange(ckin,1,20) &&
+                Util.inRange(ckout-1,1,20) &&
+                ckin < ckout);
+    }
 
-	private boolean overlapped(
-			int ckin1,int ckout1,int ckin2,int ckout2) {
-		if (ckin1 > ckin2) {
-			int t;
-			t = ckin2;  ckin2  = ckin1;  ckin1 = t;
-			t = ckout2; ckout2 = ckout1; ckout1 = t;
-		}
-		return (ckin2 < ckout1);
-	}
+    private boolean overlapped(
+        int ckin1,int ckout1,int ckin2,int ckout2) {
+        if (ckin1 > ckin2) {
+            int t;
+            t = ckin2;  ckin2  = ckin1;  ckin1 = t;
+            t = ckout2; ckout2 = ckout1; ckout1 = t;
+        }
+        return (ckin2 < ckout1);
+    }
 
-	// 重複検査
-	public void add(Reservation r) {
-		Reservation r2;
-		if (!valid(r)) {
-			messageView.append(r+" is invalid"); return;
-		}
-		if ((r2 = model.checkDuplicate(r.owner, r.ckin, r.ckout))!=null) {
-			messageView.append(r+" is duplicated with "+r2); return;
-		}
-		model.add(r);
-	}
+    // 重複検査
+    public void add(Reservation r) {
+        Reservation r2;
+        if (!valid(r)) {
+            messageView.append(r+" is invalid"); return;
+        }
+        if ((r2 = model.checkDuplicate(r.owner, r.ckin, r.ckout))!=null) {
+            messageView.append(r+" is duplicated with "+r2); return;
+        }
+        model.add(r);
+    }
 
-	public void remove(Reservation r) {
-		// TODO: 検査
-		// model.remove(r);
-	}
+    public void remove(Reservation r) {
+        // TODO: 検査
+        // model.remove(r);
+    }
 
-	// TODO: リストの走査
-	public void update() {
-		resvView.update();
-		roomView.update();
-	}
+    // TODO: リストの走査
+    public void update() {
+        resvView.update();
+        roomView.update();
+    }
 }
--- src/main/java/main/control/Util.java
+++ src/main/java/main/control/Util.java
@@ -6,30 +6,29 @@ import main.model.Reservation;
 
 // staticメソッドのみ
 public class Util {
-	public static  int toInt(String s) {
-		int v=0;
-		try {
-			v = Integer.parseInt(s);
-		} catch (NumberFormatException e) {
-			v = -1;
-		}
-		return v;
-	}
+    public static  int toInt(String s) {
+        int v=0;
+        try {
+            v = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            v = -1;
+        }
+        return v;
+    }
 
-	// 範囲検査:l,uは正, 数字以外はvは-1なので失敗
-	public static  boolean inRange(int v, int l, int u) {
-		return  l <= v  && v <= u;
-	}
+    // 範囲検査:l,uは正, 数字以外はvは-1なので失敗
+    public static  boolean inRange(int v, int l, int u) {
+        return  l <= v  && v <= u;
+    }
 
-	private static String[] sv = new String[5];
-	public static String[] generate(int id, Reservation r) {
-		Arrays.fill(sv, "");
-		sv[0] = String.format("%d", id);
-		sv[1] = r.owner;
-		sv[2] = r.ckin;
-		sv[3] = r.ckout;
-		sv[4] = r.num;
-		return sv;
-	}
-
+    private static String[] sv = new String[5];
+    public static String[] generate(int id, Reservation r) {
+        Arrays.fill(sv, "");
+        sv[0] = String.format("%d", id);
+        sv[1] = r.owner;
+        sv[2] = r.ckin;
+        sv[3] = r.ckout;
+        sv[4] = r.num;
+        return sv;
+    }
 }
